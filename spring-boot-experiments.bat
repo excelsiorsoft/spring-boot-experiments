71e0ac4d42f1e0dd19c3854d883215a6be4014e6
sleyzerzon <senya22@yahoo.com>
2020-02-08 13:26:30 -0500

    fixed launch script

--- spring-boot-experiments.bat
+++ spring-boot-experiments.bat
@@ -1,2 +1,2 @@
 
-start /B C:\"Program Files"\SpringTools4\sts-4.5.0.RELEASE\SpringToolSuite4.exe -data %~dp0 -clean -showlocation -vmC:\Java\jdk1.8.0_144\bin\java.exe -vmargs -Xmx1024m -XX:MaxPermSize=256m -vmargs -javaagent:lombok.jar
+start /B C:\"Program Files"\SpringTools4\sts-4.5.0.RELEASE\SpringToolSuite4.exe -data %~dp0 -clean -showlocation -vm C:\Java\jdk1.8.0_144\bin\java.exe -vmargs -Xmx1024m  
